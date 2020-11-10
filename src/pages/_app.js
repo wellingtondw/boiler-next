76a8434595f243042e1b0ce2ba1a5667be9e20ce
wellington <wellingtonslpbi@hotmail.com>
2020-11-10 20:48:55 -0300

    create global style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import Main from 'components/Main'
+
 export default function Home() {
-  return <p>Hello World</p>
+  return <Main />
 }
